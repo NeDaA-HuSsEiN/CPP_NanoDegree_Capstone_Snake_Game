073e3c46c4af9b9542a4bd74634cc6926d7a533c
Nedaa Hussein <nedaa.hussein@thirdwayv.com>
2025-01-10 07:26:24 +0200

    Cont apply the logic

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -8,7 +8,7 @@ Renderer::Renderer(const std::size_t screen_width,
     : screen_width(screen_width),
       screen_height(screen_height),
       grid_width(grid_width),
-      grid_height(grid_height) {
+      grid_height(grid_height){
   // Initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cerr << "SDL could not initialize.\n";
@@ -27,6 +27,7 @@ Renderer::Renderer(const std::size_t screen_width,
 
   // Create renderer
   sdl_renderer = SDL_CreateRenderer(sdl_window, -1, SDL_RENDERER_ACCELERATED);
+
   if (nullptr == sdl_renderer) {
     std::cerr << "Renderer could not be created.\n";
     std::cerr << "SDL_Error: " << SDL_GetError() << "\n";
@@ -38,7 +39,7 @@ Renderer::~Renderer() {
   SDL_Quit();
 }
 
-void Renderer::Render(Snake const snake, SDL_Point const &food) {
+void Renderer::Render(Snake const snake, SDL_Point const &food, SDL_Point const &bonus_food, bool show_bonus_food, std::vector<SDL_Point> const &obstacles) {
   SDL_Rect block;
   block.w = screen_width / grid_width;
   block.h = screen_height / grid_height;
@@ -47,14 +48,37 @@ void Renderer::Render(Snake const snake, SDL_Point const &food) {
   SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
   SDL_RenderClear(sdl_renderer);
 
+  //Render obstacles
+  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x88, 0xFF);
+  for (auto const &obstacle : obstacles) {
+    block.x = obstacle.x * block.w;
+    block.y = obstacle.y * block.h;
+    SDL_RenderFillRect(sdl_renderer, &block);
+  }
+  
   // Render food
   SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
   SDL_RenderFillRect(sdl_renderer, &block);
 
+  // Render bonus food if active
+  if (show_bonus_food) {
+    SDL_SetRenderDrawColor(sdl_renderer, 0x00, 0xFF, 0x00, 0xFF); // Green color
+    block.w = screen_width / grid_width;
+    block.h = screen_height / grid_height;
+    block.x = bonus_food.x * block.w;
+    block.y = bonus_food.y * block.h;
+    SDL_RenderFillRect(sdl_renderer, &block);
+   }
+  
   // Render snake's body
-  SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  // if snake get poisoned render a green body
+  if(snake.poison_snake){
+    SDL_SetRenderDrawColor(sdl_renderer, 0xBE, 0xD3, 0x3D, 0xFF); // green color
+  }else{
+    SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+  }
   for (SDL_Point const &point : snake.body) {
     block.x = point.x * block.w;
     block.y = point.y * block.h;
@@ -70,12 +94,20 @@ void Renderer::Render(Snake const snake, SDL_Point const &food) {
     SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
   }
   SDL_RenderFillRect(sdl_renderer, &block);
-
+ 
   // Update Screen
   SDL_RenderPresent(sdl_renderer);
 }
 
-void Renderer::UpdateWindowTitle(int score, int fps) {
-  std::string title{"Snake Score: " + std::to_string(score) + " FPS: " + std::to_string(fps)};
+void Renderer::UpdateWindowTitle(std::string player_name, int score, int fps, int high_score) {
+  std::string title{"Player name: "+ player_name+ " Score: " + std::to_string(score) + " FPS: " + std::to_string(fps) + " Player High Score: " + std::to_string(high_score)};
   SDL_SetWindowTitle(sdl_window, title.c_str());
+}
+
+
+void Renderer::RenderPauseOverlay() {
+
+  std::string title{"Paused, click P again to resume playing"};
+  SDL_SetWindowTitle(sdl_window, title.c_str());
+  SDL_RenderPresent(sdl_renderer);  
 }
--- src/renderer.h
+++ src/renderer.h
@@ -2,8 +2,10 @@
 #define RENDERER_H
 
 #include <vector>
+#include <string>
 #include "SDL.h"
 #include "snake.h"
+#include "obstacle.h"
 
 class Renderer {
  public:
@@ -11,9 +13,10 @@ class Renderer {
            const std::size_t grid_width, const std::size_t grid_height);
   ~Renderer();
 
-  void Render(Snake const snake, SDL_Point const &food);
-  void UpdateWindowTitle(int score, int fps);
-
+  void Render(Snake const snake, SDL_Point const &food, SDL_Point const &bonus_food, bool show_bonus_food, std::vector<SDL_Point> const &obstacles);
+  void UpdateWindowTitle(std::string player_name, int score, int fps, int high_score);
+  void RenderPauseOverlay();
+    
  private:
   SDL_Window *sdl_window;
   SDL_Renderer *sdl_renderer;
